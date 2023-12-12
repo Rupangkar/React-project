139d2fd574091d07a3c1f7bf0a61f91dee459456
Developer Rupangkar <developeu.rupangkar@gmail.com>
2023-12-12 16:35:57 +0600

    test perpous

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     <div className="max-w-md">
       <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
       <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <button className="btn btn-primary">Get Startedd</button>
     </div>
   </div>
 </div>
